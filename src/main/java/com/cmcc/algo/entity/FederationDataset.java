9d14c4709a8f19fa4cba1aca98168e86bda459c9
xuxu <xuxu@email.com>
2020-06-19 16:10:56 +0800

    add federation dataset

---
+++ src/main/java/com/cmcc/algo/entity/FederationDataset.java
@@ -0,0 +1,100 @@
+package com.cmcc.algo.entity;
+
+import lombok.Data;
+//import lombok.EqualsAndHashCode;
+//import lombok.experimental.Accessors;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import java.util.Date;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author hjy
+ * @since 2020-05-25
+ */
+//@EqualsAndHashCode(callSuper = false)
+//@Accessors(chain = true)
+@ApiModel
+@Data
+@Entity
+@Table(name = "tb_federation_dataset")
+public class FederationDataset implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 数据ID
+     */
+    @Id
+    @GeneratedValue
+    @Column
+    @ApiModelProperty(value = "自增id")
+    private Short id;
+
+    /**
+     * 上传文件路径
+     */
+    @Column
+    @ApiModelProperty(value = "文件名字")
+    private String name;
+
+    /**
+     * 文件更新时间
+     */
+    @Column
+    @ApiModelProperty(value = "文件更新时间")
+    private Date updatedAt;
+
+    /**
+     * 数据大小
+     */
+    @Column
+    @ApiModelProperty(value = "数据大小")
+    private String size;
+
+    /**
+     * 数据行数
+     */
+    @Column
+    @ApiModelProperty(value = "数据行数")
+    private Integer rows;
+
+    /**
+     * 数据party
+     */
+    @Column
+    @ApiModelProperty(value = "userId")
+    private Integer userId;
+
+    /**
+     * 联邦ID
+     */
+    @Column
+    @ApiModelProperty(value = "联邦ID")
+    private String federationUuid;
+
+    /**
+     * 数据类别
+     */
+    @Column
+    @ApiModelProperty(value = "数据类别")
+    private Short type;
+
+    public FederationDataset(String federationUuid, Integer userId) {
+        this.federationUuid = federationUuid;
+        this.userId = userId;
+    }
+}
---
+++ src/main/java/com/cmcc/algo/mapper/FederationDatasetRepository.java
@@ -0,0 +1,21 @@
+package com.cmcc.algo.mapper;
+
+import com.cmcc.algo.entity.FederationDataset;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author hjy
+ * @since 2020-05-25
+ */
+@Repository
+public interface FederationDatasetRepository extends JpaRepository<FederationDataset, Long> {
+    List<FederationDataset> findByFederationUuidAndType(String federationUuid, Short type);
+}
